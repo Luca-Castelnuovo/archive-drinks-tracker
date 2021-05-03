6274ebaa25768080843482aba84089a599a797c5
Luca Castelnuovo <luca@castelnuovo.xyz>
2021-05-04 00:53:41 +0200

    Update codestyke

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
-use App\Controllers\GeneralController;
-use App\Controllers\UserController;
 use App\Controllers\AuthWebhookController;
 use App\Controllers\DrinksController;
+use App\Controllers\GeneralController;
+use App\Controllers\UserController;
 use App\Middleware\AuthKeyMiddleware;
 use CQ\Controllers\AuthCodeController;
 use CQ\Controllers\AuthDeviceController;
@@ -15,11 +15,11 @@ use CQ\Middleware\JsonMiddleware;
 
 $route->get('/', [GeneralController::class, 'index']);
 
-$middleware->create(['middleware' => [FormMiddleware::class]], function () use ($route, $middleware) {
+$middleware->create(['middleware' => [FormMiddleware::class]], static function () use ($route): void {
     $route->post('/upload', [GeneralController::class, 'upload']);
 });
 
-$middleware->create(['prefix' => '/auth'], function () use ($route, $middleware) {
+$middleware->create(['prefix' => '/auth'], static function () use ($route, $middleware): void {
     $route->get('/request', [AuthCodeController::class, 'request']);
     $route->get('/callback', [AuthCodeController::class, 'callback']);
     $route->get('/logout', [AuthCodeController::class, 'logout']);
@@ -27,18 +27,18 @@ $middleware->create(['prefix' => '/auth'], function () use ($route, $middleware)
     $route->get('/request/device', [AuthDeviceController::class, 'request']);
     $route->post('/callback/device', [AuthDeviceController::class, 'callback']);
 
-    $middleware->create(['middleware' => [JsonMiddleware::class]], function () use ($route) {
+    $middleware->create(['middleware' => [JsonMiddleware::class]], static function () use ($route): void {
         $route->post('/delete', [AuthWebhookController::class, 'delete']);
     });
 });
 
-$middleware->create(['middleware' => [AuthMiddleware::class]], function () use ($route) {
+$middleware->create(['middleware' => [AuthMiddleware::class]], static function () use ($route): void {
     $route->get('/dashboard', [UserController::class, 'dashboard']);
     $route->get('/authkey', [UserController::class, 'createAuthKey']);
 });
 
-$middleware->create(['middleware' => [AuthKeyMiddleware::class]], function () use ($route, $middleware) {
-    $middleware->create(['middleware' => [JsonMiddleware::class]], function () use ($route) {
+$middleware->create(['middleware' => [AuthKeyMiddleware::class]], static function () use ($route, $middleware): void {
+    $middleware->create(['middleware' => [JsonMiddleware::class]], static function () use ($route): void {
         $route->post('/submit', [DrinksController::class, 'create']);
     });
 });
