26e693521d862b38d286e560bad1caeccb2d10a4
Luca Castelnuovo <luca@castelnuovo.xyz>
2021-05-05 13:40:09 +0200

    order drink output

--- app/Models/Drink.php
+++ app/Models/Drink.php
@@ -31,14 +31,32 @@ final class Drink
                 timestamp: strtotime(datetime: $drink['created_at'])
             );
 
+            $time = date(
+                format: 'H:i',
+                timestamp: strtotime(datetime: $drink['created_at'])
+            );
+
+            $drink['created_at'] = $time;
+
+            // If day doesn't exist in $drinks, create new day and add drink
             if (!array_key_exists(key: $day, array: $drinks)) {
                 $drinks[$day] = [
                     $drink
                 ];
+
                 continue;
             }
 
             array_push($drinks[$day], $drink);
+        }
+
+        // Order drinks by time
+        foreach ($drinks as $key => $drink) {
+            $created_at = array_column($drink, 'created_at');
+            array_multisort($created_at, SORT_ASC, $drink);
+
+
+            $drinks[$key] = $drink;
         }
 
         return $drinks;
